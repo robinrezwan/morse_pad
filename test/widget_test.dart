6b8c83e8bdcf6897e5c6724c1f2e03b30b48a4cf
Robin Rezwan <34566398+robinrezwan@users.noreply.github.com>
2022-05-20 23:05:14 +0600

    Project structure updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:morse_pad/src/main.dart';
+import 'package:morse_pad/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
